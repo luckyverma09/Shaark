625b7dac826450b205bdbd2a56ae6825efcdbfaa
Lucky Verma <idonthaveemail1af@gmail.com>
2024-02-25 01:41:31 +0530

    slider complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Use class names to select the elements
+//navbar
+
 let searchBtn = document.querySelector(".searchBtn");
 let closeBtn = document.querySelector(".closeBtn");
 let searchBox = document.querySelector(".searchBox");
@@ -27,3 +28,55 @@ menuToggle.onclick = function () {
   closeBtn.classList.remove("active");
   searchBtn.classList.remove("active");
 };
+
+//slider
+
+let items = document.querySelectorAll(".slider .list .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let thumbnails = document.querySelectorAll(".thumbnail .item");
+
+
+let countItem = items.length;
+let itemActive = 0;
+
+next.onclick = function () {
+  itemActive = itemActive + 1;
+  if (itemActive >= countItem) {
+    itemActive = 0;
+  }
+  showSlider();
+};
+prev.onclick = function () {
+  itemActive = itemActive - 1;
+  if (itemActive < 0) {
+    itemActive = countItem - 1;
+  }
+  showSlider();
+};
+let refreshInterval = setInterval(() => {
+  next.click();
+}, 5000);
+function showSlider() {
+  // remove item active old
+  let itemActiveOld = document.querySelector(".slider .list .item.active");
+  let thumbnailActiveOld = document.querySelector(".thumbnail .item.active");
+  itemActiveOld.classList.remove("active");
+  thumbnailActiveOld.classList.remove("active");
+
+  items[itemActive].classList.add("active");
+  thumbnails[itemActive].classList.add("active");
+
+  // clear auto time run slider
+  clearInterval(refreshInterval);
+  refreshInterval = setInterval(() => {
+    next.click();
+  }, 5000);
+}
+
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", () => {
+    itemActive = index;
+    showSlider();
+  });
+});
