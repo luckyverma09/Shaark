bb122708848763e11dea0f9d56ac74ed0a32536e
Lucky Verma <idonthaveemail1af@gmail.com>
2024-03-03 20:24:08 +0530

    change div name

--- script.js
+++ script.js
@@ -113,3 +113,15 @@ function addAnimation() {
     });
   });
 }
+
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+
+// create the scrollSmoother before your scrollTriggers
+ScrollSmoother.create({
+  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  effects: true, // looks for data-speed and data-lag attributes on elements
+  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
