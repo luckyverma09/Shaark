e7caa4958af4467531e6d5f08563177461785a5f
Lucky Verma <idonthaveemail1af@gmail.com>
2024-03-03 23:44:58 +0530

    Remove commented out code and unused dependencies

--- script.js
+++ script.js
@@ -28,84 +28,21 @@ menuToggle.onclick = function () {
   searchBtn.classList.remove("active");
 };
 
-
-//slider
-
-// let items = document.querySelectorAll(".slider .list .item");
-// let next = document.getElementById("next");
-// let prev = document.getElementById("prev");
-// let thumbnails = document.querySelectorAll(".thumbnail .item");
-
-
-// let countItem = items.length;
-// let itemActive = 0;
-
-// next.onclick = function () {
-//   itemActive = itemActive + 1;
-//   if (itemActive >= countItem) {
-//     itemActive = 0;
-//   }
-//   showSlider();
-// };
-// prev.onclick = function () {
-//   itemActive = itemActive - 1;
-//   if (itemActive < 0) {
-//     itemActive = countItem - 1;
-//   }
-//   showSlider();
-// };
-// let refreshInterval = setInterval(() => {
-//   next.click();
-// }, 5000);
-// function showSlider() {
-  
-  
-//   let itemActiveOld = document.querySelector(".slider .list .item.active");
-//   let thumbnailActiveOld = document.querySelector(".thumbnail .item.active");
-//   itemActiveOld.classList.remove("active");
-//   thumbnailActiveOld.classList.remove("active");
-
-//   items[itemActive].classList.add("active");
-//   thumbnails[itemActive].classList.add("active");
-
-
-//   clearInterval(refreshInterval);
-//   refreshInterval = setInterval(() => {
-//     next.click();
-//   }, 5000);
-// }
-
-// thumbnails.forEach((thumbnail, index) => {
-//   thumbnail.addEventListener("click", () => {
-//     itemActive = index;
-//     showSlider();
-//   });
-// });
-
-
-
 // servies
-
 
 const scrollers = document.querySelectorAll(".scroller");
 
-// If a user hasn't opted in for recuded motion, then we add the animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
 
 function addAnimation() {
   scrollers.forEach((scroller) => {
-    // add data-animated="true" to every `.scroller` on the page
     scroller.setAttribute("data-animated", true);
 
-    // Make an array from the elements within `.scroller-inner`
     const scrollerInner = scroller.querySelector(".scroller__inner");
     const scrollerContent = Array.from(scrollerInner.children);
 
-    // For each item in the array, clone it
-    // add aria-hidden to it
-    // add it into the `.scroller-inner`
     scrollerContent.forEach((item) => {
       const duplicatedItem = item.cloneNode(true);
       duplicatedItem.setAttribute("aria-hidden", true);
@@ -113,15 +50,3 @@ function addAnimation() {
     });
   });
 }
-
-
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-
-// create the scrollSmoother before your scrollTriggers
-ScrollSmoother.create({
-  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
-  effects: true, // looks for data-speed and data-lag attributes on elements
-  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
-});
