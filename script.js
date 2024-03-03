2832a4892531fbb46a673e28f7d64da3e80c20d4
Lucky Verma <idonthaveemail1af@gmail.com>
2024-03-03 15:43:50 +0530

    service section added to homepage

--- script.js
+++ script.js
@@ -31,53 +31,85 @@ menuToggle.onclick = function () {
 
 //slider
 
-let items = document.querySelectorAll(".slider .list .item");
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
-let thumbnails = document.querySelectorAll(".thumbnail .item");
+// let items = document.querySelectorAll(".slider .list .item");
+// let next = document.getElementById("next");
+// let prev = document.getElementById("prev");
+// let thumbnails = document.querySelectorAll(".thumbnail .item");
 
 
-let countItem = items.length;
-let itemActive = 0;
+// let countItem = items.length;
+// let itemActive = 0;
 
-next.onclick = function () {
-  itemActive = itemActive + 1;
-  if (itemActive >= countItem) {
-    itemActive = 0;
-  }
-  showSlider();
-};
-prev.onclick = function () {
-  itemActive = itemActive - 1;
-  if (itemActive < 0) {
-    itemActive = countItem - 1;
-  }
-  showSlider();
-};
-let refreshInterval = setInterval(() => {
-  next.click();
-}, 5000);
-function showSlider() {
+// next.onclick = function () {
+//   itemActive = itemActive + 1;
+//   if (itemActive >= countItem) {
+//     itemActive = 0;
+//   }
+//   showSlider();
+// };
+// prev.onclick = function () {
+//   itemActive = itemActive - 1;
+//   if (itemActive < 0) {
+//     itemActive = countItem - 1;
+//   }
+//   showSlider();
+// };
+// let refreshInterval = setInterval(() => {
+//   next.click();
+// }, 5000);
+// function showSlider() {
   
   
-  let itemActiveOld = document.querySelector(".slider .list .item.active");
-  let thumbnailActiveOld = document.querySelector(".thumbnail .item.active");
-  itemActiveOld.classList.remove("active");
-  thumbnailActiveOld.classList.remove("active");
+//   let itemActiveOld = document.querySelector(".slider .list .item.active");
+//   let thumbnailActiveOld = document.querySelector(".thumbnail .item.active");
+//   itemActiveOld.classList.remove("active");
+//   thumbnailActiveOld.classList.remove("active");
 
-  items[itemActive].classList.add("active");
-  thumbnails[itemActive].classList.add("active");
+//   items[itemActive].classList.add("active");
+//   thumbnails[itemActive].classList.add("active");
 
 
-  clearInterval(refreshInterval);
-  refreshInterval = setInterval(() => {
-    next.click();
-  }, 5000);
+//   clearInterval(refreshInterval);
+//   refreshInterval = setInterval(() => {
+//     next.click();
+//   }, 5000);
+// }
+
+// thumbnails.forEach((thumbnail, index) => {
+//   thumbnail.addEventListener("click", () => {
+//     itemActive = index;
+//     showSlider();
+//   });
+// });
+
+
+
+// servies
+
+
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
 }
 
-thumbnails.forEach((thumbnail, index) => {
-  thumbnail.addEventListener("click", () => {
-    itemActive = index;
-    showSlider();
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
   });
-});
+}
