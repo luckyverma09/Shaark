75891dcaab6e51bc7128dbf5315e959e5e2e45dc
Lucky Verma <idonthaveemail1af@gmail.com>
2024-03-08 14:46:29 +0530

    header fixed

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
 //navbar
 
-// let searchBtn = document.getElementById("searchBtn");
-// let closeBtn = document.getElementById("closeBtn");
-// let searchBox = document.querySelector(".searchBox");
-// let menuToggle = document.querySelector(".menuToggle");
-// let header = document.querySelector("header");
+let menuToggle = document.querySelector(".menuToggle");
+let header = document.querySelector("header");
 
-// searchBtn.onclick = function () {
-//   searchBox.classList.add("active");
-//   closeBtn.classList.add("active");
-//   searchBtn.classList.add("active");
-//   menuToggle.classList.add("hide");
-//   header.classList.remove("open");
-// };
+menuToggle.onclick = function () {
+  header.classList.toggle("open");
+};
 
-// closeBtn.onclick = function () {
-//   searchBox.classList.remove("active");
-//   closeBtn.classList.remove("active");
-//   searchBtn.classList.remove("active");
-//   menuToggle.classList.remove("hide");
-// };
-
-// menuToggle.onclick = function () {
-//   header.classList.toggle("open");
-//   searchBox.classList.remove("active");
-//   closeBtn.classList.remove("active");
-//   searchBtn.classList.remove("active");
-// };
 
 // servies
 
